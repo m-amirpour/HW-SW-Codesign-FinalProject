fd5148e6d0d49489d50b96483b6ab455c0a30662
= <=>
2026-02-10 18:05:03 +0330

    compute each penguin separately

--- systemc_model/systemc_model/epc_accelerator.cpp
+++ systemc_model/systemc_model/epc_accelerator.cpp
@@ -22,7 +22,6 @@ void EPC_Accelerator::get_new_position(const double current_pair[2], const doubl
     double theta_j = std::atan2(y_j, x_j);
 
     double theta_k = (1.0 / b) * std::log((1 - Q) * std::exp(b * theta_i) + Q * std::exp(b * theta_j));
-
     double r_k = a * std::exp(b * theta_k);
 
     new_pair[0] = r_k * std::cos(theta_k);
@@ -45,41 +44,74 @@ void EPC_Accelerator::get_adjusted_position(const double current[DIM], const dou
         new_pos[i] = new_pair[0];
         new_pos[j] = new_pair[1];
     }
-
 }
 
 void EPC_Accelerator::accelerate_process() {
-    std::uniform_real_distribution<double> noise_dist(-1.0, 1.0);
-
     std::cout << "Hardware accelerator ready." << std::endl;
 
     while (true) {
-        UpdatePacket packet = in_port.read();
+        pkt = in_port.read();
 
-        for (int i = 0; i < POP_SIZE; ++i) {
-            if (i == packet.best_idx) continue;
-
-            double current[DIM];
-            std::copy(&packet.current_pop[i * DIM], &packet.current_pop[i * DIM + DIM], current);
-
-            double this_Q = packet.Q[i];
-
-            double temp_pos[DIM];
-            get_adjusted_position(current, packet.best_pos, this_Q, packet.b, packet.a, temp_pos, rng);
-
-            // Add exploration noise
-            for (int d = 0; d < DIM; ++d) {
-                double noise_val = noise_dist(rng) * (packet.ub[d] - packet.lb[d]) * packet.exploration_rate;
-                temp_pos[d] += noise_val;
-                temp_pos[d] = std::max(packet.lb[d], std::min(packet.ub[d], temp_pos[d]));
+        for (int i = 0; i < POP_SIZE; i += BATCH_SIZE) {
+            for (int w = 0; w < BATCH_SIZE; ++w) {
+                batch_indices[w] = i + w;
             }
 
-            // Write back
-            std::copy(temp_pos, temp_pos + DIM, &packet.current_pop[i * DIM]);
+            // Toggle start signal to create a value-change event
+            batch_toggle = !batch_toggle;
+            start_batch.write(batch_toggle);
+            
+            // Wait 1ns for workers to see the signal change
+            wait(1, SC_NS);
 
-            wait(10, SC_NS);  // Simulate per-penguin hardware latency
+            // Wait for all workers to finish
+            for (int w = 0; w < BATCH_SIZE; ++w) {
+                while (worker_done[w].read() != batch_toggle) {
+                    wait(1, SC_NS);
+                }
+            }
         }
 
-        out_port.write(packet);
+        out_port.write(pkt);
+    }
+}
+
+void EPC_Accelerator::compute_one_penguin_position(int worker_id) {
+    std::uniform_real_distribution<double> noise_dist(-1.0, 1.0);
+    bool last_seen = start_batch.read();
+
+    while (true) {
+        // Wait for start_batch to change value
+        while (start_batch.read() == last_seen) {
+            wait(1, SC_NS);
+        }
+        last_seen = start_batch.read();
+
+        int i = batch_indices[worker_id];
+
+        if (i >= POP_SIZE || i == pkt.best_idx) {
+            worker_done[worker_id].write(last_seen);
+            continue;
+        }
+
+        double current[DIM];
+        std::copy(&pkt.current_pop[i * DIM], &pkt.current_pop[i * DIM + DIM], current);
+
+        double this_Q = pkt.Q[i];
+
+        double temp_pos[DIM];
+        get_adjusted_position(current, pkt.best_pos, this_Q, pkt.b, pkt.a, temp_pos, rng[worker_id]);
+
+        for (int d = 0; d < DIM; ++d) {
+            double noise_val = noise_dist(rng[worker_id]) * (pkt.ub[d] - pkt.lb[d]) * pkt.exploration_rate;
+            temp_pos[d] += noise_val;
+            temp_pos[d] = std::max(pkt.lb[d], std::min(pkt.ub[d], temp_pos[d]));
+        }
+
+        std::copy(temp_pos, temp_pos + DIM, &pkt.current_pop[i * DIM]);
+
+        wait(10, SC_NS);  // Simulate HW latency
+
+        worker_done[worker_id].write(last_seen);
     }
 }
